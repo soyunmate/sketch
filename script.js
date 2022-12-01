a9becd4534cba458549f0093526b64c11a73b93c
soyunmate <gm.barahona911@gmail.com>
2022-12-01 19:55:37 -0300

    Refactor script and css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 const btnRender = document.querySelector(".btn-squares");
+const divContainer = document.querySelector(".container");
 
 const createDiv = function (classes, text = "") {
   const div = document.createElement("div");
@@ -7,24 +8,6 @@ const createDiv = function (classes, text = "") {
   div.textContent = text;
   return div;
 };
-
-const divContainer = createDiv("container");
-const square = createDiv("square", "placeholder text");
-console.log(divContainer);
-console.log(square);
-
-document.querySelector("body").insertAdjacentElement("beforeEnd", divContainer);
-
-// for (let i = 1; i <= 16 * 16; i++) {
-//   const div = createDiv("square");
-//   divContainer.append(div);
-
-//   div.addEventListener("mouseover", function () {
-//     div.classList.add("animated-square");
-//   });
-// }
-
-let numberOfSquares = 16;
 
 const diplaySquares = function (number) {
   for (let i = 1; i <= number ** 2; i++) {
@@ -37,4 +20,20 @@ const diplaySquares = function (number) {
   }
 };
 
-btnRender.addEventListener("click", diplaySquares.bind(null, numberOfSquares));
+const renderContainer = function (n) {
+  divContainer.style.width = `${n * 10}px`;
+  divContainer.style.height = `${n * 10}px`;
+};
+
+const selectAndDisplaySquares = function () {
+  const squareNumber = parseInt(
+    prompt("Enter the number of squares per side you want (1-100)")
+  );
+  if (squareNumber > 100) return alert("not a valid number");
+
+  document.querySelector(".container").innerHTML = "";
+  renderContainer(squareNumber);
+  diplaySquares(squareNumber);
+};
+
+btnRender.addEventListener("click", selectAndDisplaySquares);
